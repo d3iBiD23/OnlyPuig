c086a28be36fd3f1c88ded633c22d90c787aa7d7
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2025-03-11 21:59:00 +0100

    Update CommentsFragment.java

--- app/src/main/java/com/example/onlypuig/CommentsFragment.java
+++ app/src/main/java/com/example/onlypuig/CommentsFragment.java
@@ -149,7 +149,6 @@ public class CommentsFragment extends Fragment {
                             Map<String, Object> originalData = doc.getData();
                             Map<String, Object> minimalComment = new HashMap<>();
                             minimalComment.put("$id", doc.getId()); // Agrega el ID del comentario
-                            minimalComment.put("uid", originalData.get("uid"));
                             minimalComment.put("author", originalData.get("author"));
                             minimalComment.put("content", originalData.get("content"));
                             minimalComment.put("createdAt", originalData.get("createdAt"));
@@ -181,7 +180,6 @@ public class CommentsFragment extends Fragment {
 
                 // Preparamos el nuevo comentario usando el nombre real del usuario
                 Map<String, Object> newComment = new HashMap<>();
-                newComment.put("uid", userResult.getId().toString());
                 newComment.put("postId", postId);
                 newComment.put("author", userName);
                 newComment.put("content", commentText);
